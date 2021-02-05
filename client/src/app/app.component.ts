5ea5f8c6c6693f83276d5d94f413d967ce5d4f99
Bruce Lai <bruceplai@yahoo.com>
2021-02-04 16:07:35 -0800

    Add sort, filter, pagination to title data table

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { ITitle } from './title';
 import { TitleService } from './title.service';
 
@@ -10,16 +12,25 @@ import { TitleService } from './title.service';
 })
 export class AppComponent implements OnInit {
   public title = 'netflix';
-  public displayedColumns: string[] = ['title', 'type', 'director', 'cast'];
+  public displayedColumns: string[] = ['title', 'type', 'category', 'director', 'cast'];
   public dataSource = new MatTableDataSource<ITitle>();
 
   constructor(private titleService: TitleService) {}
 
   private getData() {
     this.titleService.getTitles().subscribe(response => {
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
       this.dataSource.data = response;
     });
   }
+
+  public doFilter(value: string) {
+    this.dataSource.filter = value.trim().toLocaleLowerCase()
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit() {
     this.getData();
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatTableModule } from '@angular/material/table';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

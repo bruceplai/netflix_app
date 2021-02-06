818a0207c9d08858d42af5ea9e4ccd3fd978933a
Bruce Lai <bruceplai@yahoo.com>
2021-02-05 23:03:54 -0800

    Client: Move angular material modules to their own module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,14 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTableModule } from '@angular/material/table';
-import { MatCardModule } from '@angular/material/card';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { FlexModule } from '@angular/flex-layout';
 import { TruncatePipe } from './truncate.pipe';
 import { ReplaceEmpty } from './replace-empty.pipe';
@@ -19,6 +11,9 @@ import { TitleTableComponent } from './title-table/title-table.component';
 import { TitleDetailComponent } from './title-table/title-detail/title-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormsModule } from '@angular/forms';
+import { ChartComponent } from './chart/chart.component';
+import { MaterialModule } from './material/material.module';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [
@@ -28,21 +23,16 @@ import { FormsModule } from '@angular/forms';
     TitleTableComponent,
     TitleDetailComponent,
     DashboardComponent,
+    ChartComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatTableModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
     FlexModule,
     FormsModule,
+    MaterialModule,
+    NgxChartsModule,
     RouterModule.forRoot([
       { path: 'search', component: TitleTableComponent },
       { path: 'stats', component: DashboardComponent },
---
+++ client/src/app/material/material.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  imports: [
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  exports: [
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ]
+})
+export class MaterialModule {}

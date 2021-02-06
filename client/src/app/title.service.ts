402c354688b74ee3cac1108e044eeda692f5177f
Bruce Lai <bruceplai@yahoo.com>
2021-02-05 22:04:53 -0800

    Client: Add new service methods

--- client/src/app/title.service.ts
+++ client/src/app/title.service.ts
@@ -1,19 +1,87 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { ITitle } from './title';
+import { IDataPoint } from './data-point';
 import { Observable } from 'rxjs';
 
-const API_BASE = 'http://localhost:9090/';
+const API_BASE = 'http://localhost:9090';
 
 @Injectable({ providedIn: 'root' })
 export class TitleService {
   constructor(private http: HttpClient) {}
 
-  public getTitles(): Observable<ITitle[]> {
-    return this.http.get<ITitle[]>(API_BASE + 'title');
+  public getTitles(
+    title: string = null,
+    director: string = null
+  ): Observable<ITitle[]> {
+    let params = new HttpParams();
+    if (title) {
+      params = params.append('title', title);
+    }
+    if (director) {
+      params = params.append('director', director);
+    }
+    return this.http.get<ITitle[]>(API_BASE + '/title', { params: params });
   }
 
   public getTitle(id: string): Observable<ITitle> {
-    return this.http.get<ITitle>(API_BASE + 'title' + id);
+    const params = new HttpParams({ fromObject: { id: id } });
+    return this.http.get<ITitle>(API_BASE + '/title', { params: params });
+  }
+
+  public getYears(
+    title: string = null,
+    director: string = null
+  ): Observable<IDataPoint[]> {
+    let params = new HttpParams();
+    if (title) {
+      params = params.append('title', title);
+    }
+    if (director) {
+      params = params.append('director', director);
+    }
+    return this.http.get<IDataPoint[]>(API_BASE + '/year', { params: params });
+  }
+
+  public getRatings(
+    title: string = null,
+    director: string = null
+  ): Observable<IDataPoint[]> {
+    let params = new HttpParams();
+    if (title) {
+      params = params.append('title', title);
+    }
+    if (director) {
+      params = params.append('director', director);
+    }
+    return this.http.get<IDataPoint[]>(API_BASE + '/rating', { params: params });
+  }
+
+  public getGenres(
+    title: string = null,
+    director: string = null
+  ): Observable<IDataPoint[]> {
+    let params = new HttpParams();
+    if (title) {
+      params = params.append('title', title);
+    }
+    if (director) {
+      params = params.append('director', director);
+    }
+    return this.http.get<IDataPoint[]>(API_BASE + '/genre', { params: params });
+  }
+
+  public getCountries(
+    title: string = null,
+    director: string = null
+  ): Observable<IDataPoint[]> {
+    let params = new HttpParams();
+    if (title) {
+      params = params.append('title', title);
+    }
+    if (director) {
+      params = params.append('director', director);
+    }
+    return this.http.get<IDataPoint[]>(API_BASE + '/country', { params: params });
   }
 }
